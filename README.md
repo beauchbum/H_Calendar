f9f18522a729e16dcf440518ac565bfda6f422f0
Ryan <ryanbeauchamp93@gmail.com>
2017-10-12 16:24:29 -0500

    Finished Prototype

---
+++ README.md
@@ -0,0 +1,62 @@
+# Hopper Homework Assignment - Methods and Assumptions
+
+## Process and Timeline
+
+#### Friday, Oct. 6
+
+Isabella sent me the homework assignment and corresponding data. 
+
+#### Sunday, Oct. 8
+
+I quarantined myself from NFL football and read through the assignment. I then started a Jupyter Notebook so that I could load the data with Pandas and begin some initial exploration. I verified that all the data was Boston to Cancun - round trip and then identified unique values for various columns and found areas of prevelant NA (refundable being one). 
+
+After formulating an initial strategy, I began to create the HTML/CSS skeleton for the calendar. I mostly finished the skeleton by the end of the day. 
+
+#### Monday, Oct. 9
+
+Made the decision that I would stick to data from Oct. 1 2016 to Sept. 30 2017 as it was much more dense than the data for queries 2017-2018. I used Pandas to peform various GroupBys and min() aggregations to find the cheapest possible flight for each relevant departure date.
+
+Wrote the results to a static .csv file that was then used as an input to the Python code for manipulation. The manipulation created the list of lists of lists that would outline the calendar and be passed to the HTML skeleton. 
+
+
+#### Tuesday, Oct. 10
+
+Did not have much time to work on the assignment, but I did some tweaks to the styling/layout of the skeleton. Created custom Bootstrap buttons for orange/red and also worked on element padding to avoid being cut off on phones. 
+
+
+#### Wednesday, Oct. 11
+
+Created .csv file that would outline the possible return dates given a departure date. Worked on pulling that into the python enviornment and building logic to pass data back and forth between Server and Client. 
+
+
+#### Thursday, Oct. 12
+
+
+Scrapped the static .csv files and instead uploaded a modified version of the original dataset (kept only relevant columns) to a Google CloudSQL database. Altered the Python logic to query from SQL rather than read in .csv. 
+
+
+## Design Decisions
+
+#### Price Intervals
+
+Created the intervals by subtracting the minimum price from the 75th percentile price and then dividing by four. There was no mathematical basis for this, but it was the result of some tinkering to arrive at intervals that I believed were in line with Hopper's design.
+
+#### CSV vs SQL
+
+Quickly found out that the file was too big to be loaded each time the prototype was started. For that reason I gravitated towards creating compacted CSV files and presenting it as "This is a static calender as if it is Oct. 8, 2016". However, I had some extra time on Thursday to upload to SQL and realized I could make the calendar dynamic by doing Group Bys in the query rather than in Python. 
+
+#### Round Trip vs. One Way
+
+Did not attempt to tease out any one way prices based on the various round trip flights. While this is probably not true to real life, I operated under the assumption that flights could only be purchased as a package. This limited the possible return dates given a selected departure date. 
+
+#### 2016-2017 Data
+
+As mentioned above, I found the 2016-2017 data to have much more information and thought it would lend itself better to filling in the calendar. The application takes today's date and subtracts a year so that it operates in 2016. 
+
+#### Received Date
+
+When determining departure/return prices, the application filters out any information with a received_date in the future. For example if it is Oct. 12, the app will not consider any rows with received_date of Oct. 13 or more. 
+
+I did not take into consideration the received_ms even though in production this would be a constraint. 
+
+
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import Flask, request, redirect, g, render_template, make_response, u
 import csv
 import os
 import numpy as np
+import MySQLdb
+from datetime import datetime, timedelta
 
-
+### Running in local environment or deployed with Google App Engine
 if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
     pass
 else:
@@ -14,31 +16,79 @@ app.config.update(
     SEND_FILE_MAX_AGE_DEFAULT=3600
 )
 
+### Global variables - Used to hold values sent back to server from JavaScript
 dates_dict = {}
 intervals = []
 departure_date = ""
 all_dates = []
 
 
+### Connection specs for CloudSQL database
+host = '35.202.134.73'
+pw = "hopper_pw"
+port = 3306
+user = 'root'
+name = 'hopper'
+CLOUDSQL_PROJECT = "hopper-182321"
+CLOUDSQL_INSTANCE = "hopper-182321:us-central1:hopper-sql"
 
+### Function to create SQL connection
+def sql_connect():
+    conn = MySQLdb.connect(unix_socket='/cloudsql/{}:{}'.format(CLOUDSQL_PROJECT, CLOUDSQL_INSTANCE), user=user,
+                           host=host, passwd=pw, db=name)
+    return conn
+
+
+### Main route - handles heavy lifting of grabbing data from SQL and initializing calendar
 @app.route("/", methods=["POST", "GET"])
 def index():
+    ### Holds the pricing intervals for coloring dates [Price1, Price2, Price3, Price4]
     global intervals
+
+    ### Main array to be passed to client when rendering Index.html
     calendar_data = []
+
+    ### Used to map date to month - ex. 2016-10-24 == 'OCT'
     month_dict = {1: "JAN", 2: "FEB", 3: "MAR", 4: "APR", 5: "MAY", 6: "JUN", 7: "JUL", 8: "AUG", 9: "SEP", 10: "OCT",
                   11: "NOV", 12: "DEC"}
 
+
+    ### Array to hold departure data from SQL
     imported_data = []
-    rt_imported_data = []
 
-    with open('departure_data.csv', 'rb') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        spamreader.next()
-        for row in spamreader:
-            imported_data.append(row)
+    ### Find today's date and then use Python datetime to go back one year
+    last_year_date = datetime.now().replace(year=datetime.now().year - 1)
 
+    ### Set calendar bounds - hard coded to Oct. 1 2016 and Sept. 30 2017
+    first_date = datetime.strptime("2016-10-01", '%Y-%m-%d').date()
+    last_date = datetime.strptime("2017-09-30", '%Y-%m-%d').date()
 
-    # Create the calendar with departure prices
+    ### Create array of all dates in between and including the calendar bounds
+    dd = [first_date + timedelta(days=x) for x in range((last_date - first_date).days + 1)]
+
+    ### Connect to CloudSQL and then query to get relevant departure dates and minimum price via group by
+    conn = sql_connect()
+    cursor = conn.cursor()
+    query = "select departure_odate, min(total_usd) from flights where departure_odate>=%s and departure_odate<=%s and received_date<=%s group by departure_odate"
+    param = [last_year_date, last_date, last_year_date]
+    cursor.execute(query, param)
+    sql_departure = cursor.fetchall()
+
+    ### Create combination values of all dates between first/end bounds (dd) then add prices from sql_departure
+    i = 0
+    j = 0
+    while i < len(dd):
+        if j < len(sql_departure):
+            if dd[i]== sql_departure[j][0]:
+                imported_data.append((str(dd[i]), dd[i].weekday(), int(sql_departure[j][1])))
+                j += 1
+            else:
+                imported_data.append((str(dd[i]), dd[i].weekday(), ""))
+        else:
+            imported_data.append((str(dd[i]), dd[i].weekday(), ""))
+        i += 1
+
+    ### Create a list of lists of lists [months[weeks[days]]] that will represent the calendar
     month_list = []
     departure_prices = []
     for i in imported_data:
@@ -57,21 +107,30 @@ def index():
                 calendar_data.append(chunks)
             month = int(date_split[1])
             month_list = []
-            dow = int(i[1])
 
+            ### Python's Date.weekday() goes M,T,W,T,F,S,S while calendar is S,M,T,W,T,F,S
+            dow = int(i[1])
+            if dow == 6:
+                dow = 0
+            else:
+                dow+=1
+
+            ### Insert blanks when necessary to correctly align month start/end dates
             for k in range(0, 7):
-                if (k - 1) == dow:
+                if k == dow:
                     month_list.append(month_dict[month])
                 else:
                     month_list.append("")
-            for l in range(0, dow + 1):
+            for l in range(0, dow):
                 month_list.append("BLANK")
+
+        ### If departing flight exists for that date, append price, otherwise append blank
         if i[-1] != "":
             month_list.append((i[0],str(int(date_split[2])),float(i[-1])))
         else:
             month_list.append((i[0],str(int(date_split[2])), i[-1]))
 
-    # Need to add in the last month
+    ### Need to add in the last month as the loop above ends before
     chunks = [month_list[x:x + 7] for x in xrange(0, len(month_list), 7)]
     if len(chunks[-1]) != 7:
         temp = len(chunks[-1])
@@ -79,23 +138,25 @@ def index():
             chunks[-1].append("BLANK")
     calendar_data.append(chunks)
 
-    with open('round_trip_data.csv', 'rb') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        spamreader.next()
-        for row in spamreader:
-            dates_dict[row[0]].append((row[1],row[2]))
+    ### Create a dictionary of departure/return flights. Dict[Departure Date] = [(ReturnDate1, Price1), (ReturnDate2, Price2), etc...]
+    query = "select departure_odate, return_odate, min(total_usd) from flights where departure_odate>=%s and departure_odate<=%s and received_date<=%s group by departure_odate, return_odate"
+    param = [last_year_date, last_date, last_year_date]
+    cursor.execute(query, param)
+    for row in [(str(x[0]), str(x[1]), int(x[2])) for x in cursor.fetchall()]:
+        dates_dict[row[0]].append((row[1], row[2]))
 
-
-    # Create price distribution
+    ### Create the price distribuion. Find the min price as lower bound, then add created interval to get the others
     min = np.min(departure_prices)
     rounded_min = min - (min % 10)
-    median = np.median(departure_prices)
-    max = np.max(departure_prices)
     step = round((np.percentile(departure_prices,75) - min)/4,-1)
     intervals = [int(x*step+rounded_min) for x in range(4)]
 
     return render_template("index.html", calendar=calendar_data, intervals = intervals)
 
+
+### Route to handle selecting a departure
+### Sets the global "departue_date" for use later
+### Finds all possible return dates with the "dates_dict"
 @app.route("/selectDeparture", methods=["GET"])
 def selectDeparture():
     global departure_date
@@ -104,6 +165,9 @@ def selectDeparture():
     return_dates = dates_dict[date]
     return jsonify(return_dates = return_dates)
 
+
+### Route to handle selecting a return
+### Find the dates in between so they can be shaded light blue
 @app.route("/selectReturn", methods=["GET"])
 def selectReturn():
     return_date = request.args['date']
