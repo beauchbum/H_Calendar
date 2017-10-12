fbd5527dae3fb73c24657605dd94f07041e48775
Ryan <ryanbeauchamp93@gmail.com>
2017-10-12 17:05:19 -0500

    removing instance from gitignore?

---
+++ instance/config.py
@@ -0,0 +1,9 @@
+# config.py
+DEBUG = True
+DBHOST = '35.202.134.73'
+DBPASS = "hopper_pw"
+DBPORT = 3306
+DBUSER = 'root'
+DBNAME = 'hopper'
+CLOUDSQL_PROJECT = "hopper-182321"
+CLOUDSQL_INSTANCE =  "us-central1:hopper-sql"
